4970ffa917288c65a1b00cac8e1b40c8ef796bb3
md ataullah <121761337+ataullah454@users.noreply.github.com>
2023-08-29 00:49:25 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,37 @@
+document.getElementById(`mood`).addEventListener(`click`, function(){
+    if(document.documentElement.classList.contains(`dark`)){
+      document.documentElement.classList.remove(`dark`);
+      document.getElementById(`mood`).innerHTML = `<i class="fa-solid fa-moon"></i>`
+    }else{
+      document.documentElement.classList.add(`dark`);
+      document.getElementById(`mood`).innerHTML = `<i class="fa-solid fa-sun"></i>`
+    }
+  })
+
+
+
+  function show(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }  
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClock").innerText = time;
+    document.getElementById("MyClock").textContent = time;
+    setTimeout(show, 1000);  
+}
+show();
